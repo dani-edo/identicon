76e382d69c57db18a5a8d8a752bd15c645f416a7
dani_edo <dani.edogawa.17@gmail.com>
2024-07-28 13:42:46 +0700

    pass mirror_row function to map and refacor mirror_row function

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -9,14 +9,14 @@ defmodule Identicon do
   def build_grid(%Identicon.Image{hex: hex}) do
     hex
     |> Enum.chunk_every(3)
+    |> Enum.map(&mirror_row/1)
   end
 
   def mirror_row(row) do
-    # [145, 46, 200]
-    [first, second | _tail] = row
-
-    # [145, 46, 200, 46, 145]
-    row ++ [second, first]
+    case row do
+      [first, second | _tail] -> row ++ [second, first]
+      _ -> row
+    end
   end
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
